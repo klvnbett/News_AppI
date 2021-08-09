362d77582507ac59cf728c88e8d5274ed8f62935
BettK <kelvin.bett@student.moringaschool.com>
2021-08-09 13:51:15 +0300

    added the api key

--- app/views.py
+++ app/views.py
@@ -5,15 +5,16 @@ from app import app
 @app.route('/')
 def index():
 
-    '''
+     '''
     View root page function that returns the index page and its data
     '''
-    message = "Hello world"
-    return render_template('index.html',message=message)
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
 
 
-@app.route('/news/<news_id>')
-def movie(news_id):
+@app.route('/news/<int:news_id>')
+def news(news_id):
 
     '''
     View news page function that returns the news details page and its data
---
+++ instance/config.py
@@ -0,0 +1,1 @@
+NEWS_API_KEY ='7d18ecd749d24ca3930cffe2f7a3c6cc'
