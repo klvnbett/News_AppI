3d56301d4be23c6237bceb048028189097f705d7
BettK <kelvin.bett@student.moringaschool.com>
2021-08-09 14:00:50 +0300

    added class for news instance

---
+++ app/models/news.py
@@ -0,0 +1,10 @@
+class News:
+    '''
+    news class to define news Objects
+    '''
+
+    def __init__(self,id,title,author,description):
+        self.id =id
+        self.title = title        
+        self.author = author
+        self.description = description
--- app/views.py
+++ app/views.py
@@ -5,11 +5,11 @@ from app import app
 @app.route('/')
 def index():
 
-     '''
-    View root page function that returns the index page and its data
     '''
+    View root page function that returns the index page and its data
+        '''
 
-    title = 'Home - Welcome to The best Movie Review Website Online'
+    title = 'NEWS - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title)
 
 
