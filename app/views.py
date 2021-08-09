23f1a008ba39e2eba3c1b6e0ba9a9f218cc31702
BettK <kelvin.bett@student.moringaschool.com>
2021-08-09 13:45:59 +0300

    added the call message

--- app/views.py
+++ app/views.py
@@ -8,4 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = "Hello world"
+    return render_template('index.html',message=message)
+
+
+@app.route('/news/<news_id>')
+def movie(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
